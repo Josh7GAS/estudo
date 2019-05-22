303ef5cf3c4eff64972a6bea9d6315b0fbdfdb28
joshua <joshua@nubeliu.com>
2019-05-22 14:53:37 -0300

    pratica em shell

---
+++ shellscript/primeiroprograma.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#Esse Primeiro programa para mostrar conceitos básicos
+#
+
+
+NOME="Joshua"
+echo "ola mundo" ${NOME}
+NOME="gustavoi"
+echo "ola ${NOME}"
+
+echo "Qual é o seu nome?" 
+read MEU_NOME
+echo "ola" ${MEU_NOME}
---
+++ shellscript/terceiro.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Um Simples Programa de argumentos
+
+echo "Argumento" $1
+echo "Argumento" $2
+
+#numero de argumentos passados
+
+echo "Forma passados um total de $# argumentos"
+
+#lista de todos os argumentos
+
+echo A lista de argumentos a seguir $@
