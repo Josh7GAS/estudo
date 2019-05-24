e1548626a189297f19639909637f021bfdc93ff8
joshua <joshua@nubeliu.com>
2019-05-24 14:39:25 -0300

    teste_supervisor

---
+++ teste_supervisor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Imprimir a primeira colunba do Docker
+DOCKER = `sudo docker ps -a |awk -F ' ' '{print $1, "\t\t"}'`
+
+echo "${DOCKER}"
+
+#Organizar saida em variaveis 
+
+#Imprimir dentro de uma estrutura JSON
+
+# echo -n '{"data":['
+
+# for 
