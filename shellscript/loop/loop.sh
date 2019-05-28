f27e956eda54eb1f031a18b8e8794a606876af1a
Joshua Sousa <joshua@nubeliu.com>
2019-05-28 09:32:56 -0300

    shellScript finalizado

--- shellscript/loop/loop.sh
+++ shellscript/loop/loop.sh
@@ -4,3 +4,12 @@ do
 	echo "Número da Vez será: ${numero}"
 
 done
+
+TESTE=4
+
+for numero in {1..${TESTE}}
+do
+
+echo "Olá": ${numero}
+
+done
--- teste_supervisor.sh
+++ teste_supervisor.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 #Organizar saida em variaveis 
-DOCKER=`sudo docker ps -a |awk -F ' ' '{print $1, "\t\t"}'`
-COUNTER=`sudo docker ps -a |awk -F ' ' '{print $1, "\t\t"}'|wc -l`
-echo ${DOCKER}
-echo ${COUNTER}
+DOCKER=$(sudo docker ps -a |awk -F ' ' '{print $1, "\t\t"}')
+COUNTER=$(sudo docker ps -a |awk -F ' ' '{print $1, "\t\t"}'|wc -l)
+LINHA=$(sudo docker ps -a |awk '{print$1}'|head -n1 | tail -1)
+CONSTANTE=$(seq 1 ${COUNTER})
+
+
+
+# echo ${DOCKER}
+# echo ${COUNTER}
+# echo ${LINHA}
+# echo ${CONSTANTE}
 echo -n '{"data":['
 
 
 
-for var1 in ${COUNTER}
+for VAR1 in ${DOCKER};
     do
-    echo -n "{\"{NOME-DO-SEU-CORAÇÃO}\": \"${DOCKER}\"},"
+  
+    # LINHA=$(sudo docker ps -a |awk '{print$1}'|head -n${VAR1} | tail -1)
+    echo -n "{\"{Tipo}\": \"${VAR1}\"},"
+    
 done | sed -e 's:\},$:\}:'
 
-echo -n ']}'
+# echo -n ']}'
 
 
-Untitled 
-echo -n '{"data":['
-​
-for (TE VIRA)
-	(TE VIRA)
-	(TE VIRA)
-  echo -n "{\"{NOME-DO-SEU-CORAÇÃO}\": \"${VARIAVEL-DO-SEU-CORAÇÃO}\"},"
-done | sed -e 's:\},$:\}:'
-​
-echo -n ']}'
+
+# echo -n '{"data":['
+# ​
+# for (TE VIRA)
+# 	(TE VIRA)
+# 	(TE VIRA)
+#   echo -n "{\"{NOME-DO-SEU-CORAÇÃO}\": \"${VARIAVEL-DO-SEU-CORAÇÃO}\"},"
+# done | sed -e 's:\},$:\}:'
+# ​
+# echo -n ']}'
 
 
-{
-  "data": [
-    {
-      "{#NOME-DO-SEU-CORAÇÃO}": "CONTAINER"
-    },
-    {
-      "{#NOME-DO-SEU-CORAÇÃO}": "fd86cc03c681"
-    }
-  ]
-}
+# {
+#   "data": [
+#     {
+#       "{#NOME-DO-SEU-CORAÇÃO}": "CONTAINER"
+#     },
+#     {
+#       "{#NOME-DO-SEU-CORAÇÃO}": "fd86cc03c681"
+#     }
+#   ]
+# }
