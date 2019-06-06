4b8f4bc7cb3d807e676aab26c554fd800deb542f
Joshua Sousa <joshua@nubeliu.com>
2019-06-06 08:22:51 -0300

    exercicios de shell

--- shellscript/exercicios/exercicio1.sh
+++ shellscript/exercicios/exercicio1.sh
@@ -4,25 +4,34 @@
 function ola(){
 echo "Poderia me dar seu nome e sua idade?"
 echo "Qual é o seu nome?"
-read NOME
+read NOMEA
 echo "Qual é a sua idade?"
-read IDADE
+read IDADEA
 
 #segunda pessoa
 echo "Me de mais um nome"
-read NOME
+read NOMEB
 echo "Qual é a idade dessa pessoa?"
-read IDADE
+read IDADEB
 
 #Terceira pessoa
 echo "Me outro nome"
-read NOME
+read NOMEC
 echo "Qual é a idade dessa pessoa?"
-read IDADE
+read IDADEC
 }
 
 ola
 #comparando a idade das 3 pessoas 
+function comparacao(){
+if [[ ${IDADEA} -g ${IDADEB} ||  ]]
+then 
+echo "${NOMEA} é o mais velho ${NOMEB}"
+else 
+if [[ ${IDADEB} -g "é o mais velho" ${IDADEC} ]]
+fi
 
+if [[ ${}]]
+}
 
 #mostrar o resultado das comparações
